c3908142935dde633fce630e97bd6ebeaa7cdecf
kimsh02 <kimshawn02@icloud.com>
2025-11-10 02:28:39 -0700

    Edit schema and .gitignore

--- src/Database/Sql/Schema.sql
+++ src/Database/Sql/Schema.sql
@@ -1,38 +1,42 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE IF NOT EXISTS migrations (
-  version    UINTEGER PRIMARY KEY,
-  applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE MACRO string_length_ok (s) AS length (s) > 0
+AND length (s) <= 300;
 
-CREATE TABLE IF NOT EXISTS settings (
-  id               UUID PRIMARY KEY DEFAULT uuidv7(),
-  target_lang_idx  UINTEGER NOT NULL,
-);
+CREATE TABLE
+    IF NOT EXISTS migrations (
+        version UINTEGER PRIMARY KEY,
+        applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
 
-CREATE TABLE IF NOT EXISTS decks (
-  id           UUID PRIMARY KEY DEFAULT uuidv7(),
-  name         VARCHAR NOT NULL UNIQUE
-);
+CREATE TABLE
+    IF NOT EXISTS settings (
+        id UUID PRIMARY KEY DEFAULT uuidv7 (),
+        target_language_idx UINTEGER NOT NULL,
+    );
 
-CREATE TABLE IF NOT EXISTS cards (
-  id               UUID PRIMARY KEY DEFAULT uuidv7(),
-  deck_id          UUID NOT NULL REFERENCES decks(id),
-  created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at       TIMESTAMP,
-  reviewed_at      TIMESTAMP,
+CREATE TABLE
+    IF NOT EXISTS decks (
+        id UUID PRIMARY KEY DEFAULT uuidv7 (),
+        name VARCHAR NOT NULL UNIQUE CHECK (string_length_ok (name))
+    );
 
-  difficulty       DOUBLE NOT NULL,
-  stability        DOUBLE NOT NULL,
-
-  front_text       VARCHAR NOT NULL,
-  front_norm_text  VARCHAR NOT NULL,
-  back_text        VARCHAR NOT NULL,
-  back_norm_text   VARCHAR NOT NULL,
-  audio_path       VARCHAR,
-  image_path       VARCHAR,
-
-  UNIQUE(deck_id, front_text)
-);
+CREATE TABLE
+    IF NOT EXISTS cards (
+        id UUID PRIMARY KEY DEFAULT uuidv7 (),
+        deck_id UUID NOT NULL REFERENCES decks (id),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP,
+        reviewed_at TIMESTAMP,
+        difficulty DOUBLE NOT NULL,
+        stability DOUBLE NOT NULL,
+        front_text VARCHAR NOT NULL CHECK (string_length_ok (front_text)),
+        front_norm_text VARCHAR NOT NULL CHECK (string_length_ok (front_norm_text)),
+        back_text VARCHAR NOT NULL CHECK (string_length_ok (back_text)),
+        back_norm_text VARCHAR NOT NULL CHECK (string_length_ok (back_norm_text)),
+        audio_path VARCHAR CHECK (string_length_ok (audio_path)),
+        image_path VARCHAR CHECK (string_length_ok (image_path)),
+        UNIQUE (deck_id, front_text)
+    );
 
 COMMIT;
